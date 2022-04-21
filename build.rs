b220c87d5725dacf0e0c47eeaadcfb0b7a59e33d
Balamurali Pandranki <balamurali@live.com>
2022-04-21 21:58:29 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,11 +2,11 @@ use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(
         &["fhir/proto/google/fhir/proto/annotations.proto"],
-        &["fhir/proto"],
+        &["fhir/proto/google/fhir/proto/"],
     )?;
     prost_build::compile_protos(
         &["fhir/proto/google/fhir/proto/profile_config.proto"],
-        &["fhir/proto"],
+        &["fhir/proto/google/fhir/proto/"],
     )?;
     Ok(())
 }
