756f6fd85cd3a334eec0468d8f9b0cde2f266c76
Balamurali Pandranki <balamurali@live.com>
2022-04-21 22:00:32 +0530

    Update build.rs

--- build.rs
+++ build.rs
@@ -4,9 +4,9 @@ fn main() -> Result<()> {
         &["fhir/proto/google/fhir/proto/annotations.proto"],
         &["fhir/proto/google/fhir/proto/"],
     )?;
-    prost_build::compile_protos(
-        &["fhir/proto/google/fhir/proto/profile_config.proto"],
-        &["fhir/proto/google/fhir/proto/"],
-    )?;
+    // prost_build::compile_protos(
+    //     &["fhir/proto/google/fhir/proto/profile_config.proto"],
+    //     &["fhir/proto/google/fhir/proto/"],
+    // )?;
     Ok(())
 }
